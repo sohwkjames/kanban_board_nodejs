1217c86c97e9efc65ca8ba76cbbeee840d52181b
unknown <jamessoh11@gmail.com>
2023-07-24 12:40:32 +0800

    Fix promote task 2 done

--- microservices/promoteTask2Done.js
+++ microservices/promoteTask2Done.js
@@ -1,128 +1,214 @@
 const bcrypt = require("bcryptjs");
 const { config } = require("../utils/dbConfig");
 const mysql = require("mysql");
+const { connect } = require("../routes/routes");
+const dayjs = require("dayjs");
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 // Settings
 const saltRounds = 10;
 connection = mysql.createConnection(config);
 
 async function promoteTask2Done(req, res, next) {
-    const { username, password, taskId, taskNote, appAcronym } = req.body;
+  const { username, password, taskId, taskNote, appAcronym } = req.body;
 
-    if (!username || !password || !taskId || !appAcronym) {
-        return res.status(200).send({
-            code: "Mandatory parameters are missing",
-        });
-    }
-
-    const user = await new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM accounts WHERE username=?`;
-        connection.query(sql, [username], (err, results) => {
-            if (err) reject(err);
-            console.log("james", results);
-            if (results.length) {
-                console.log("james", results);
-                resolve(results[0]);
-            } else {
-                resolve(false);
-            }
-        });
-    });
-
-    if (!user) {
-        return res.status(200).send({
-            code: "Invalid credential",
-        });
-    }
-
-    const validPassword = await bcrypt.compare(password, user.password);
-
-    if (!validPassword) {
-        return res.status(200).send({
-            code: "Invalid credential",
-        });
-    }
-
-    if (user.isActive === 0) {
-        return res.status(200).send({
-            code: "Inactive user",
-        });
-    }
-
-    const userGroupsArr = await new Promise((resolve, reject) => {
-        const sql = "SELECT usergroup FROM username_usergroup_pivot WHERE username = ? ";
-        connection.query(sql, [user.username], (err, results) => {
-            if (err) reject(err);
-            const tmpArr = results.map((record) => record.usergroup);
-            resolve(tmpArr);
-        });
-    });
-
-    const isAppValid = await new Promise((resolve, reject) => {
-        const sql = "SELECT * FROM application WHERE App_Acronym = ?";
-        connection.query(sql, [appAcronym], (err, results) => {
-            if (err) reject(err);
-            if (results.length === 0) {
-                resolve(false);
-            } else {
-                resolve(true);
-            }
-        });
-    });
-
-    if (!isAppValid) {
-        return res.status(200).send({
-            code: "Invalid app acronym",
-        });
-    }
-
-    const appObject = await new Promise((resolve, reject) => {
-        const sql = "SELECT * FROM application WHERE App_Acronym = ?";
-        connection.query(sql, [appAcronym], (err, results) => {
-            if (err) reject(err);
-            resolve(results[0]);
-        });
-    });
-
-    // Compare userGroupsArr vs application app_premit_doing group
-    if (!userGroupsArr.includes(appObject.App_permit_doing)) {
-        return res.status(200).send({
-            code: "You do not have permission to perform this action",
-        });
-    }
-
-    const taskObj = await new Promise((resolve, reject) => {
-        const sql = "SELECT * FROM task WHERE Task_id = ?";
-        connection.query(sql, [taskId], (err, results) => {
-            if (err) reject(err);
-            resolve(results[0]);
-        });
-    });
-
-    if (!taskObj) {
-        return res.status(200).send({
-            code: "Invalid task id",
-        });
-    }
-
-    if (taskObj.Task_state !== "doing") {
-        return res.status(200).send({
-            code: "Task is not at doing state",
-        });
-    }
-
-    const newTaskObj = await new Promise((resolve, reject) => {
-        const sql = "UPDATE task SET Task_state=? WHERE Task_id=?";
-        connection.query(sql, ["done", taskId], (err, results) => {
-            if (err) reject(err);
-            resolve(results[0]);
-        });
-    });
-
+  if (!username || !password || !taskId || !appAcronym) {
     return res.status(200).send({
-        taskId,
-        code: "Successfully promoted task to done",
+      code: "Mandatory parameters are missing",
     });
+  }
+
+  const user = await new Promise((resolve, reject) => {
+    const sql = `SELECT * FROM accounts WHERE username=?`;
+    connection.query(sql, [username], (err, results) => {
+      if (err) reject(err);
+      if (results.length) {
+        resolve(results[0]);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+
+  if (!user) {
+    return res.status(200).send({
+      code: "Invalid credential",
+    });
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+
+  if (!validPassword) {
+    return res.status(200).send({
+      code: "Invalid credential",
+    });
+  }
+
+  if (user.isActive === 0) {
+    return res.status(200).send({
+      code: "Inactive user",
+    });
+  }
+
+  const userGroupsArr = await new Promise((resolve, reject) => {
+    const sql =
+      "SELECT usergroup FROM username_usergroup_pivot WHERE username = ? ";
+    connection.query(sql, [user.username], (err, results) => {
+      if (err) reject(err);
+      const tmpArr = results.map((record) => record.usergroup);
+      resolve(tmpArr);
+    });
+  });
+
+  const isAppValid = await new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM application WHERE App_Acronym = ?";
+    connection.query(sql, [appAcronym], (err, results) => {
+      if (err) reject(err);
+      if (results.length === 0) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    });
+  });
+
+  if (!isAppValid) {
+    return res.status(200).send({
+      code: "Invalid app acronym",
+    });
+  }
+
+  const appObject = await new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM application WHERE App_Acronym = ?";
+    connection.query(sql, [appAcronym], (err, results) => {
+      if (err) reject(err);
+      resolve(results[0]);
+    });
+  });
+
+  // Compare userGroupsArr vs application app_premit_doing group
+  if (!userGroupsArr.includes(appObject.App_permit_doing)) {
+    return res.status(200).send({
+      code: "You do not have permission to perform this action",
+    });
+  }
+
+  const taskObj = await new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM task WHERE Task_id = ?";
+    connection.query(sql, [taskId], (err, results) => {
+      if (err) reject(err);
+      resolve(results[0]);
+    });
+  });
+
+  if (!taskObj) {
+    return res.status(200).send({
+      code: "Invalid task id",
+    });
+  }
+
+  if (taskObj.Task_state !== "doing") {
+    return res.status(200).send({
+      code: "Task is not at doing state",
+    });
+  }
+
+  // System generated note string
+  let taskNoteString = createNoteString(
+    "System",
+    "done",
+    `${username} has promoted the task to done`
+  );
+
+  if (taskNote) {
+    taskNoteString += createNoteString(username, "done", taskNote);
+  }
+
+  const newTaskObj = await new Promise((resolve, reject) => {
+    const sql =
+      "UPDATE task SET Task_state=?, Task_notes=CONCAT(Task_notes,?) WHERE Task_id=?";
+    connection.query(sql, ["done", taskNoteString, taskId], (err, results) => {
+      if (err) reject(err);
+      resolve(results[0]);
+    });
+  });
+
+  // Handle send emails
+  const sql = `SELECT p.username, accounts.email 
+  FROM username_usergroup_pivot AS p 
+  LEFT JOIN application
+  ON p.usergroup = application.App_permit_done
+  LEFT JOIN accounts
+  ON p.username = accounts.username
+  WHERE application.App_Acronym=?`;
+
+  const emailsArr = await new Promise((resolve, reject) => {
+    connection.query(sql, [appAcronym], (err, result) => {
+      if (err) reject(err);
+      const tmp = result.map((row) => row.email);
+      const filtered = tmp.filter((name) => name !== null);
+      const string = filtered.join(",");
+      resolve(filtered);
+    });
+  });
+
+  sendEmailNotification(emailsArr, taskId);
+
+  return res.status(200).send({
+    taskId,
+    code: "Successfully promoted task to done",
+  });
 }
+
+function createNoteString(username, taskState, noteContent, timeStamp) {
+  let noteString = "";
+  noteString += username + "**" + taskState + "**" + noteContent + "**";
+  if (!timeStamp) {
+    noteString += dayjs().format("YYYY-MM-DD HH:mm:ss") + "**";
+  }
+  noteString += "||";
+
+  return noteString;
+}
+
+function noteStringToObj(str) {
+  const tmpArr = str.split("**");
+  return {
+    username: tmpArr[0],
+    taskState: tmpArr[1],
+    note: tmpArr[2],
+    timestamp: tmpArr[3],
+  };
+}
+
+async function sendEmailNotification(emailString, taskId) {
+  const mailOptions = generateMailOptions(emailString, taskId);
+
+  const result = await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) reject(err);
+      if (info) resolve(info);
+    });
+  });
+
+  return result;
+}
+
+const generateMailOptions = (recipientEmails, taskId) => {
+  return {
+    from: process.env.EMAIL_USERNAME,
+    to: recipientEmails.join(", "),
+    subject: "A new task is ready for your evaluation",
+    html: `A new task ${taskId} is ready for your evaluation.`,
+  };
+};
 
 module.exports = { promoteTask2Done };
