9ebf8a4eff0aaa931829c107b482e13af161ee6a
unknown <jamessoh11@gmail.com>
2023-07-24 10:50:13 +0800

    Finish testing on promotetask2done

--- microservices/promoteTask2Done.js
+++ microservices/promoteTask2Done.js
@@ -2,6 +2,16 @@ const bcrypt = require("bcryptjs");
 const { config } = require("../utils/dbConfig");
 const mysql = require("mysql");
 const { connect } = require("../routes/routes");
+const dayjs = require("dayjs");
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 // Settings
 const saltRounds = 10;
@@ -20,9 +30,7 @@ async function promoteTask2Done(req, res, next) {
     const sql = `SELECT * FROM accounts WHERE username=?`;
     connection.query(sql, [username], (err, results) => {
       if (err) reject(err);
-      console.log("james", results);
       if (results.length) {
-        console.log("james", results);
         resolve(results[0]);
       } else {
         resolve(false);
@@ -113,18 +121,94 @@ async function promoteTask2Done(req, res, next) {
     });
   }
 
+  // System generated note string
+  let taskNoteString = createNoteString(
+    "System",
+    "done",
+    `${username} has promoted the task to done`
+  );
+
+  if (taskNote) {
+    taskNoteString += createNoteString(username, "done", taskNote);
+  }
+
   const newTaskObj = await new Promise((resolve, reject) => {
-    const sql = "UPDATE task SET Task_state=? WHERE Task_id=?";
-    connection.query(sql, ["done", taskId], (err, results) => {
+    const sql =
+      "UPDATE task SET Task_state=?, Task_notes=CONCAT(Task_notes,?) WHERE Task_id=?";
+    connection.query(sql, ["done", taskNoteString, taskId], (err, results) => {
       if (err) reject(err);
       resolve(results[0]);
     });
   });
+
+  // Handle send emails
+  const sql = `SELECT p.username, accounts.email 
+  FROM username_usergroup_pivot AS p 
+  LEFT JOIN application
+  ON p.usergroup = application.App_permit_done
+  LEFT JOIN accounts
+  ON p.username = accounts.username
+  WHERE application.App_Acronym=?`;
+
+  const emailsArr = await new Promise((resolve, reject) => {
+    connection.query(sql, [appAcronym], (err, result) => {
+      if (err) reject(err);
+      const tmp = result.map((row) => row.email);
+      const filtered = tmp.filter((name) => name !== null);
+      const string = filtered.join(",");
+      resolve(filtered);
+    });
+  });
+
+  sendEmailNotification(emailsArr, taskId);
 
   return res.status(200).send({
     taskId,
     code: "Successfully promoted task to done",
   });
 }
+
+function createNoteString(username, taskState, noteContent, timeStamp) {
+  let noteString = "";
+  noteString += username + "**" + taskState + "**" + noteContent + "**";
+  if (!timeStamp) {
+    noteString += dayjs().format("YYYY-MM-DD HH:mm:ss") + "**";
+  }
+  noteString += "||";
+
+  return noteString;
+}
+
+function noteStringToObj(str) {
+  const tmpArr = str.split("**");
+  return {
+    username: tmpArr[0],
+    taskState: tmpArr[1],
+    note: tmpArr[2],
+    timestamp: tmpArr[3],
+  };
+}
+
+async function sendEmailNotification(emailString, taskId) {
+  const mailOptions = generateMailOptions(emailString, taskId);
+
+  const result = await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) reject(err);
+      if (info) resolve(info);
+    });
+  });
+
+  return result;
+}
+
+const generateMailOptions = (recipientEmails, taskId) => {
+  return {
+    from: process.env.EMAIL_USERNAME,
+    to: recipientEmails.join(", "),
+    subject: "A new task is ready for your evaluation",
+    html: `A new task ${taskId} is ready for your evaluation.`,
+  };
+};
 
 module.exports = { promoteTask2Done };
