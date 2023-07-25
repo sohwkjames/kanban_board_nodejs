85f857fcbb504465e71dad09c613cd11f8b620c5
unknown <jamessoh11@gmail.com>
2023-07-25 13:48:37 +0800

    Edit db config and routes

--- utils/dbConfig.js
+++ utils/dbConfig.js
@@ -1,8 +1,8 @@
 const config = {
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "a3",
+  host: "localhost",
+  user: "root",
+  password: process.env.LOCAL_DB_PASSWORD,
+  database: process.env.LOCAL_DB_DATABASE,
 };
 
 module.exports = { config };
